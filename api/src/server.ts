9ab283e10b74759617ae1e88cd73a5e3881b3512
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2021-02-23 12:20:12 -0300

    Dia 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,25 +1,12 @@
+import 'reflect-metadata'
 import express from 'express';
+import "./database";
+import { router } from './routes';
 
 const app = express();
 
-/**
- * GET => Buscar
- * POST => Salvar
- * Put => Alterar
- * DELETE => Deletar
- * PATCH => Altaração especifica
- */
+app.use(express.json()); 
 
- app.get("/", (request, response) => {
-     return response.json({message: "HELLO WORLD - NLW04"});
- });
-
- //1 param => Rota(recurso API)
- //2 param => Request Response
-
- app.post("/",(request, response) => {
-     //recebeu dados para salvar
-     return response.json({message: "Os dados foram salvos Com Sucesso."});
- });
+app.use(router);
 
 app.listen(3333,() => console.log("Server is running!"));
